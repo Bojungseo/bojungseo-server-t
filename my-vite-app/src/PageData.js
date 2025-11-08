382ff08fba631430ddb6683db5f666ae05001f57
Bojungseo <snjk1574@naver.com>
2025-11-08 21:03:02 +0900

    예정이율

---
+++ my-vite-app/src/PageData.js
@@ -0,0 +1,7 @@
+// src/userData.js
+export const users = [
+    { name: "농협손해보험", url: "https://www.nhfire.co.kr/announce/productAnnounce/applicableInterestRateAnnounce.nhfire" },
+    { name: "롯데손해보험", url: "https://www.lotteins.co.kr/web/C/D/H/cdh210.jsp" },
+    { name: "이영희", url: "https://example.net" },
+    { name: "박민수", url: "https://example.edu" },
+];
