4c932fb627fd31c9641c98a063a3316405869e38
Bojungseo <snjk1574@naver.com>
2025-11-08 21:13:18 +0900

    예정이율2

--- my-vite-app/src/PageData.js
+++ my-vite-app/src/PageData.js
@@ -1,7 +1,19 @@
 // src/userData.js
 export const users = [
-    { name: "농협손해보험", url: "https://www.nhfire.co.kr/announce/productAnnounce/applicableInterestRateAnnounce.nhfire" },
-    { name: "롯데손해보험", url: "https://www.lotteins.co.kr/web/C/D/H/cdh210.jsp" },
-    { name: "이영희", url: "https://example.net" },
-    { name: "박민수", url: "https://example.edu" },
+    { name: "농협손해보험", url: "https://www.nhfire.co.kr/announce/productAnnounce/applicableInterestRateAnnounce.nhfire", column: "left" },
+    { name: "롯데손해보험", url: "https://www.lotteins.co.kr/web/C/D/H/cdh210.jsp", column: "left" },
+    { name: "라이나손해보험", url: "https://www.chubb.com/kr-kr/disclosure/interest-rate-disclosure.html", column: "left" },
+    { name: "메리츠화재", url: "https://www.meritzfire.com/disclosure/product-announcement/interest-rate.do#!/", column: "left" },
+    { name: "삼성화재", url: "https://www.samsungfire.com/vh/page/VH.HPIF0104.do", column: "left" },
+    { name: "신한EZ손해보험", url: "https://www.shinhanez.co.kr/static/pub/PUB20020T03.html", column: "left" },
+    { name: "하나손해보험", url: "https://www.hanainsure.co.kr/w/disclosure/product/rateProduct", column: "left" },
+    { name: "한화손해보험", url: "https://www.hwgeneralins.com/notice/ir/product-ratio.do", column: "left" },
+    { name: "현대해상", url: "https://www.hi.co.kr/serviceAction.do?view=bin/PA/03/HHPA03010M", column: "left" },
+    { name: "흥국화재", url: "https://www.heungkukfire.co.kr/FRW/announce/goodsApplyInterestList.do", column: "left" },
+    { name: "캐롯손해보험", url: "https://www.hwgeneralins.com/notice/ir/product-ratio.do", column: "left" },
+    { name: "AIG손해보험", url: "https://www.aig.co.kr/wo/dpwot006.html?menuId=MS703", column: "left" },
+    { name: "AXA손해보험", url: "https://www.axa.co.kr/AsianPlatformInternet/html/axacms/common/intro/disclosure/insurance/index.html#CatIDsb13_73646", column: "left" },
+    { name: "DB손해보험", url: "https://www.idbins.com/pc/bizxpress/contentTemplet/pb/ppb/ap/mon.jsp", column: "left" },
+    { name: "KB손해보험", url: "https://www.kbinsure.co.kr/CG802060001.ec", column: "left" },
+    { name: "MG손해보험", url: "https://www.yebyeol.co.kr/PB031080DM.scp?menuId=MN0803003", column: "left" },
 ];
