453180c27e0106efd103d117c656cf976b163ee1
Bojungseo <snjk1574@naver.com>
2025-11-12 21:10:45 +0900

    Update firebase.js

--- my-vite-app/src/firebase.js
+++ my-vite-app/src/firebase.js
@@ -1,5 +1,6 @@
-// firebase.js
+// src/firebase.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -12,6 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
